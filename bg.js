7be312a90f116724d1cb11fbe28f54b2964ae0e3
FUCKHITLER <142610609+FUCKHITLER@users.noreply.github.com>
2025-03-16 02:21:26 +0000

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,3 +1,5 @@
-document.body.style.background = "url('https://cdn.discordapp.com/attachments/1315014557567287359/1350604222671880303/IMG_0634.jpg?ex=67d757b3&is=67d60633&hm=85d83dede2057a6b188ff56e80e40a02d16caa272d5789a41bd21701d1531f39&format=webp&width=396&height=857')";
-document.body.style.backgroundSize = "cover";
-console.log("Background should change now!");
+setTimeout(() => {
+    document.body.style.background = "url('https://cdn.discordapp.com/attachments/1315014557567287359/1350604222671880303/IMG_0634.jpg?ex=67d757b3&is=67d60633&hm=85d83dede2057a6b188ff56e80e40a02d16caa272d5789a41bd21701d1531f39&format=webp&width=396&height=857')";
+    document.body.style.backgroundSize = "cover";
+    console.log("Background should change now!");
+}, 3000);
