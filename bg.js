ae98250811ab2630878fc26bb76f37a12534017c
FUCKHITLER <142610609+FUCKHITLER@users.noreply.github.com>
2025-03-16 01:12:24 +0000

    Create bg.js

---
+++ bg.js
@@ -0,0 +1,2 @@
+document.body.style.background = "url('https://media.discordapp.net/attachments/1315014557567287359/1350604222671880303/IMG_0634.jpg')";
+document.body.style.backgroundSize = "cover";
