33039c3b87d26fb507a5a6557cfa86b093d17dfc
FUCKHITLER <142610609+FUCKHITLER@users.noreply.github.com>
2025-03-16 01:57:21 +0000

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.body.style.background = "url('https://media.discordapp.net/attachments/1315014557567287359/1350604222671880303/IMG_0634.jpg')";
+    document.body.style.background = "url('https://cdn.discordapp.com/attachments/1315014557567287359/1350604222671880303/IMG_0634.jpg?ex=67d757b3&is=67d60633&hm=85d83dede2057a6b188ff56e80e40a02d16caa272d5789a41bd21701d1531f39&format=webp&width=396&height=857')";
     document.body.style.backgroundSize = "cover";
+    alert("KIRO OFFICIALLY OWNS THIS SITE")
 });
