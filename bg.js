ebe8a5e954b8835cdb86641f9464fe32489a1e74
FUCKHITLER <142610609+FUCKHITLER@users.noreply.github.com>
2025-03-16 02:13:17 +0000

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,6 +1,3 @@
-window.onload = function() {
-    console.log("JavaScript is running");
-    document.body.style.background = "url('https://cdn.discordapp.com/attachments/1315014557567287359/1350604222671880303/IMG_0634.jpg?ex=67d757b3&is=67d60633&hm=85d83dede2057a6b188ff56e80e40a02d16caa272d5789a41bd21701d1531f39&format=webp&width=396&height=857')";
-    document.body.style.backgroundSize = "cover";
-    alert("KIRO OFFICIALLY OWNS THIS SITE");
-};
+document.body.style.background = "url('https://cdn.discordapp.com/attachments/1315014557567287359/1350604222671880303/IMG_0634.jpg?ex=67d757b3&is=67d60633&hm=85d83dede2057a6b188ff56e80e40a02d16caa272d5789a41bd21701d1531f39&format=webp&width=396&height=857')";
+document.body.style.backgroundSize = "cover";
+console.log("Background should change now!");
