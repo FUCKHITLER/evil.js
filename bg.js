932a238f781879509e23dc864a3038108f80adcb
FUCKHITLER <142610609+FUCKHITLER@users.noreply.github.com>
2025-03-16 01:55:56 +0000

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,2 +1,4 @@
-document.body.style.background = "url('https://media.discordapp.net/attachments/1315014557567287359/1350604222671880303/IMG_0634.jpg')";
-document.body.style.backgroundSize = "cover";
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.style.background = "url('https://media.discordapp.net/attachments/1315014557567287359/1350604222671880303/IMG_0634.jpg')";
+    document.body.style.backgroundSize = "cover";
+});
